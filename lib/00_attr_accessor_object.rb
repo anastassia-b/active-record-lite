e3f48073ad571cf36df305ca6d917c0c8bd29a00
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-06-16 10:56:24 -0700

    Add attr_accessor macro

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,18 @@
+# implement a ::my_attr_accessor macro, which should do exactly the same
+#   thing as the real attr_accessor: it should define setter/getter methods.
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |val|
+        instance_variable_set("@#{name}", val)
+      end
+
+    end
   end
 end
